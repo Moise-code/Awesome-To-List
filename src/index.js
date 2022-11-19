dcf959d1b63801958ecdb8961f6503a207d0901d
Moise-code <moisewowe@gmail.com>
2022-11-19 16:57:07 +0200

    adding the codes to remove the activity

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import { Alert } from 'bootstrap';
 import './style.css';
 const form = document.querySelector('.add');
 const list = document.querySelector('.list-group')
+const spanning = document.querySelector('.spanning')
+
 
 // secondly, we are going to create a function which will get the html format of a new todo list and append it to ui
 // we pass the input value as the parameter since is the variable contains the value of input.
 const addHtml = (inputValue) =>{
     // to get the entered input
-    const html = `<li class="list-group-item d-flex justify-content-between align-items-center text-light">
-    <span>${inputValue}</span>
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+    <div>
+    <input type="checkbox" class=" m-0 checking" name="check">
+    <span class ="spanning">${inputValue}</span>
+    </div>
     <i class="far fa-trash-alt delete"></i>
 </li>`;
 // then we are going to add the input to the first form which in our case is to append html variable
@@ -39,9 +45,12 @@ list.addEventListener('click', (e) =>{
     // we are going  to check if the item clicked is a trash can
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
-    }
+    }x
 // what we did above we targeted the ul(list) container
 // then we checked to see if it contains class called delete
 // after that we then targeted the parent or container of that element which has class of delete.
 // then we added remove method to delete the element.
 });
+
+// then lets target the checkbox and one clicked the delete button will work and put under line 
+
